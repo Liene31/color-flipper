6abfb59fda619ddcf07ef1aecc0503fbd519f9be
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-07-03 11:53:17 +0200

    Refactor color selection logic and add hex color support
    Refactored the color selection logic to use a single getRandomColorIndex function for both simple and hex color arrays. Added a hexColorValues array and updated the button click handler to support switching between simple and hex color modes, updating the background and displayed color value accordingly.

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
-// The page loads as Simple
-// Simple as true and if HEX clicked changes to FALSE
-
 //HEX min #000000 max #FFFFFF
 //numbers from 0 to 9 + letters from a to f
 //random number from 0 to 9
 //random letter from a to f
 
+const simpleColors = ["#f15025", "red", "green", "rgba(133,122,200)"];
+const hexColorValues = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
 const button = document.getElementById("btn");
 const main = document.getElementById("main");
 const colorValue = document.getElementById("color-value");
-const simpleColors = ["#f15025", "red", "green", "rgba(133,122,200)"];
 const simpleLink = document.getElementById("simple-colors-link");
 const hexColorsLink = document.getElementById("hex-colors-link");
 let isSimple = true;
 
-function getRandomColorIndex() {
-  return Math.floor(Math.random() * simpleColors.length);
+function getRandomColorIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function getHexValue() {
+  let hexColor = 0;
+  for (let i = 1; i <= 6; i++) {
+    hexColor += i;
+  }
+  return hexColor;
 }
 
 //Handles the Click me Btn
 button.addEventListener("click", () => {
-  const randomNumber = getRandomColorIndex();
-  if (!isSimple) {
-    console.log("it's hex");
+  getHexValue();
+  const randomNumberSimple = getRandomColorIndex(simpleColors);
+  const randomNumberHex = getRandomColorIndex(hexColorValues);
+  if (isSimple) {
+    main.style.backgroundColor = simpleColors[randomNumberSimple];
+    colorValue.textContent = simpleColors[randomNumberSimple];
   } else {
-    main.style.backgroundColor = simpleColors[randomNumber];
-    colorValue.textContent = simpleColors[randomNumber];
+    main.style.backgroundColor = hexColorValues[randomNumberHex];
+    colorValue.textContent = `#${hexColorValues[randomNumberHex]}`;
   }
 });
 
